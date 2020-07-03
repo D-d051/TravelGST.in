f80219867c618463475a5ccefd3ae1ae3076c0c3
Dipen <d_d051@student.usc.edu.au>
2020-07-03 12:06:16 +1000

    This is TravelGST.in Coding.

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+<div class="container">
+  <div class="row align-items-center justify-content-center">
+
+    <!-- <div class="col-1">
+      <h1 class="m-0 site-logo"><a href="index.php">TravelGST</a></h1>
+    </div> -->
+
+    <div class="col-12 ">
+      <nav class="site-navigation position-relative text-left" role="navigation">
+        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+          <li><a href="#home-section" class="nav-link">Home</a></li>
+          <li><a href="#about-section" class="nav-link">About</a></li>
+          <li><a href="#services-section" class="nav-link">Solution</a></li>
+          <li><a href="#projects-section" class="nav-link">GST Automation</a></li>
+          <li><a href="#option-section" class="nav-link">Option and Cost</a></li>
+          <li><a href="#blog-section" class="nav-link">How It Works?</a></li>
+          <li><a href="#clients" class="nav-link">Our Cients </a></li>
+          <li><a href="#request-section" class="nav-link">Request Call Back </a></li>
+        </ul>
+      </nav>
+
+
+      <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a>
+
+    </div>
+
+  
+  </div>
+</div>
+
+</header>
