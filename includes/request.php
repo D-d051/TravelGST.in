7a8c46c5862b09202a0630598b61503c4eab83b4
Dipen <d_d051@student.usc.edu.au>
2020-07-03 12:04:15 +1000

    This is TravelGST.in Coding.

---
+++ includes/request.php
@@ -0,0 +1,56 @@
+<section class="site-section" id="request-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 mb-5 position-relative">
+            <h2 class="section-title text-center mb-5">Request A Call Back</h2>
+          </div>
+        </div>
+        <div class="row justify-content-between">
+          <div class="col-lg-8 center">
+            <form action="#" class="form">
+              <div class="row mb-4">
+                <div class="form-group col-6">
+                  <input type="text" class="form-control" placeholder="Full name" required>
+                </div>
+              </div>
+              <div class="row mb-4">
+                <div class="form-group col-12">
+                  <input type="text" class="form-control" placeholder="Company Name" name="companyName">
+                </div>
+              </div>
+
+              <div class="row mb-4">
+                <div class="form-group col-12">
+                  <input type="text" class="form-control" placeholder="Mobile Number" name="mobileNumber">
+                </div>
+              </div>
+              <div class="row mb-4">
+                <div class="form-group col-12">
+                  <input type="email" class="form-control" placeholder="Email address">
+                </div>
+              </div>
+
+              <!-- <div class="row mb-4">
+                <div class="form-group col-12">
+                  <input type="text" class="form-control" placeholder="Subject of the message">
+                </div>
+              </div>
+
+              <div class="row mb-4">
+                <div class="form-group col-12">
+                  <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Type your message here.."></textarea>
+                </div>
+              </div> -->
+
+              <div class="row">
+                <div class="col-md-6">
+                  <input type="submit" class="btn btn-primary" value="Submit">
+                </div>
+              </div>
+              
+            </form>
+          </div>
+        
+        </div>
+      </div>
+    </section>
